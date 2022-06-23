2bac04c1270450563e6721c1456da882d804dde5
Adam Rafiandri <rafiandria23@gmail.com>
2022-06-24 00:19:16 +0700

    chore: Renamed main class.

--- src/main/java/io/abover/user/AboverUserService.java
+++ src/main/java/io/abover/user/AboverUserService.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class AboverUserServiceApplication {
+public class AboverUserService {
     public static void main(String[] args) {
-        SpringApplication.run(AboverUserServiceApplication.class, args);
+        SpringApplication.run(AboverUserService.class, args);
     }
 }
